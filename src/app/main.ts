aab08651ece8552309d27de7c59b76b979f00588
Dan Wahlin <dwahlin@xmlforasp.net>
2016-03-13 23:25:26 -0700

    Added enableProcMode() option.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,9 +1,11 @@
 import { bootstrap } from 'angular2/platform/browser';
-import { bind } from 'angular2/core';
+import { bind, enableProdMode } from 'angular2/core';
 import { FORM_PROVIDERS } from 'angular2/common';
 import { ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from 'angular2/router';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import { AppComponent } from './app.component';
+
+//enableProdMode(); //Uncomment for production
 
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
